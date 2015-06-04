6a5240b805eb671950125a878ac78e5f15a6229c
Rolando Bernal <rolandobernal@Rolandos-MacBook-Pro.local>
2015-06-03 19:01:10 -0500

    Added CONTROLLERS folder and created individual files for each controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,39 +2,22 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http) {
-    $http({
-          method: 'GET',
-          url: '/api/user'
-        }).
-        success(function (data, status, headers, config) {
-          $scope.name = data.username;
-        }).
-        error(function (data, status, headers, config) {
-          $scope.name = 'Error!';
-        });
-  }).  
-  controller('HomeCtrl', function ($scope) {
-    $scope.message = "Home"
+var controllers = angular.module('myApp.controllers', [])
+controllers.controller('AppCtrl', function ($scope, $http) {
+		$http({
+					method: 'GET',
+					url: '/api/user'
+				}).
+				success(function (data, status, headers, config) {
+					$scope.name = data.username;
+				}).
+				error(function (data, status, headers, config) {
+					$scope.name = 'Error!';
+				});
+	})
 
-  }).
-  controller('AuthCtrl', function ($scope) {
-    $scope.message='NEWAuthenticateCtrl'
-    $scope.showRegister = true;
-    $scope.showLogin = false;
 
-    $scope.showLoginBtn = function(){
-      $scope.showRegister = false;
-      $scope.showLogin = true;    
-    }
 
-    $scope.showRegisterBtn = function(){
-      $scope.showRegister = true;
-      $scope.showLogin = false;    
-    }
 
-  }).
-  controller('TodoCtrl', function ($scope, $http) {
-    $scope.message = "Todo!"
-  });
+
+
